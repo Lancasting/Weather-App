17911162c31b5522fe72140899c51725aa2cb6de
Samuel Lancaster <samuellancaster66@gmail.com>
2020-04-11 19:23:28 -0600

    Pushing adjustments to HTML and JavaScript

--- Assets/javascript.js
+++ Assets/javascript.js
@@ -14,22 +14,23 @@ var searchCity = $("#city").val();
     console.log(response);
     var lat = response.coord.lat;
     var lon = response.coord.lon;
-    var temp = response.main.temp;
+    var temp = (response.main.temp - 273.15)* 9/5 +32;
     var humidity = response.main.humidity;
     var windSpeed = response.wind.speed;
     var icon = response.weather[0].icon;
     var cityName = response.name;
     var uvURL = "http://api.openweathermap.org/data/2.5/uvi?appid=a7aba1d83fecabd92ceeae64cf8f67a1&lat=" + lat + "&lon=" + lon;
+    
 
     var weatherResults = $(".weather-results");
     var title = $("<h2>").addClass(".card-title").text(cityName + Date().toString())
     weatherResults.append(title);
-    weatherResults.append(temp);
+    var temp = $("<h6>").addClass(".card-subtitle").text(temp);
+    weatherResults.append(temp + " Degrees");
+    var humidity = $("<h6>").addClass(".card-subtitle").text(humidity);
     weatherResults.append(humidity);
+    var windSpeed = $("<h6>").addClass("card-subtitle").text(windSpeed);
     weatherResults.append(windSpeed);
-
-
-
 
 
     $.ajax({
@@ -38,7 +39,8 @@ var searchCity = $("#city").val();
     }).then(function(response) {
       console.log(response);
       var uvIndex = response.value;
-
+      var uvIndex =$("<h6>").addClass(".card-subtitle").text(uvIndex);
+      weatherResults.append(uvIndex);
 
 
 
